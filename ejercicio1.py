734f1b21dd3596c82ac5ffe0c0301b1051f21669
Esteban Franco <126849354+EstebanNeorenFranco@users.noreply.github.com>
2025-09-03 19:50:53 +0000

    modified:   ejercicio1.py 	modified:   ejercicio2.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,2 +1,203 @@
-# Este archivo corresponde al Ejercicio 1 de la guía práctica sobre Clases.
-# Aquí deberás implementar la clase Camion y resolver los puntos a, b, c, d y f según las consignas
+# A - Indicá qué devuelven las siguientes expresiones. Analizalo con tus compañeros y luego ejecutá las instrucciones en la máquina para comprobar tu respuesta.
+
+# class Camion:
+#     def __init__(self, patente, marca, carga, anio): #supuse que había un error al cargar las variables así que lo modifiqué
+#         self.patente = patente
+#         self.marca = marca
+#         self.carga = carga
+#         self.anio = anio
+
+#     def __str__(self):
+#         return f"Camión: #{self.patente} \nCarga: {self.carga} \nMarca: {self.marca} \nAño: {self.anio}"
+
+# furgon1 = Camion("ABC123", "Mercedes", 1000, 2020)
+# furgon2 = furgon1
+# furgon3 = Camion("DEF456", "Volvo", 2000, 2021)
+# furgon4 = Camion("ABC123", "Mercedes", 1000, 2020)
+
+# print(furgon1 == furgon2) 
+# Da True porque solo compara identidad sin tener en cuenta el estado en memoria
+
+# print(furgon1 is furgon2) 
+# Da True porque son el mismo objeto en memoria
+
+# print(furgon3 == furgon4) 
+# Da False porque compara identidad sin tener en cuenta el estado en memoria y estos son diferentes
+
+# print(furgon3 is furgon4) 
+# Da False porque compara si son el mismo espacio en memoria y estos son diferentes
+
+# print(furgon1 == furgon4) 
+# Da Fasle porque son diferentes objetos en memoria, falta el metodo __eq__ para compararlos correctamente
+
+### B - Modificá el código dado para que la comparación de dos objetos de la clase Camion devuelva True cuando 
+### todos sus atributos sean iguales.
+
+# class Camion:
+#     def __init__(self, patente, marca, carga, anio): #supuse que había un error al cargar las variables así que lo modifiqué
+#         self.patente = patente
+#         self.marca = marca
+#         self.carga = carga
+#         self.anio = anio
+    
+#     def __eq__(self, other):
+#         if isinstance(other, Camion):
+#             return (self.patente == other.patente and
+#                     self.marca == other.marca and
+#                     self.carga == other.carga and
+#                     self.anio == other.anio)
+
+#     def __str__(self):
+#         return f"Camión: #{self.patente} \nCarga: {self.carga} \nMarca: {self.marca} \nAño: {self.anio}"
+
+# furgon1 = Camion("ABC123", "Mercedes", 1000, 2020)
+# furgon2 = furgon1
+# furgon3 = Camion("DEF456", "Volvo", 2000, 2021)
+# furgon4 = Camion("ABC123", "Mercedes", 1000, 2020)
+
+# print(furgon1 == furgon4) 
+
+### C - ¿Qué atributo hace único a nuestros objetos? Identificá el atributo que hace único al objeto Camion y 
+### modificá el código para que la comparación de dos objetos de la clase Camion devuelva True cuando ese atributo sea igual.
+
+# El atributo que hace diferente al resto es patente
+
+# class Camion:
+#     def __init__(self, patente, marca, carga, anio): #supuse que había un error al cargar las variables así que lo modifiqué
+#         self.patente = patente
+#         self.marca = marca
+#         self.carga = carga
+#         self.anio = anio
+    
+#     def __eq__(self, other):
+#         if isinstance(other, Camion):
+#             return (self.patente == other.patente)
+
+#     def __str__(self):
+#         return f"Camión: #{self.patente} \nCarga: {self.carga} \nMarca: {self.marca} \nAño: {self.anio}"
+
+# furgon1 = Camion("ABC123", "Mercedes", 1000, 2020)
+# furgon2 = furgon1
+# furgon3 = Camion("DEF456", "Volvo", 2000, 2021)
+# furgon4 = Camion("ABC123", "VW", 1000, 2020)
+
+# print(furgon1 == furgon4) 
+
+### D - Si dos personas tienen el mismo DNI, entonces... ¡Son la misma persona! ¿Cómo evitarías asignar el mismo DNI 
+### a dos personas distintas? Siguiendo esta analogía, adaptá el código anterior para el caso de los camiones.
+
+# class Camion:
+#     lista_patentes = set()
+
+#     def __init__(self, patente, marca, carga, anio): #supuse que había un error al cargar las variables así que lo modifiqué
+#         if patente in Camion.lista_patentes:
+#             raise ValueError(f'Error: La patente asignada ya está en uso')
+#         self.patente = patente
+#         self.marca = marca
+#         self.carga = carga
+#         self.anio = anio
+#         Camion.lista_patentes.add(patente)
+  
+#     def __eq__(self, other):
+#         if isinstance(other, Camion):
+#             return (self.patente == other.patente)
+
+#     def __str__(self):
+#         return f"Camión: #{self.patente} \nCarga: {self.carga} \nMarca: {self.marca} \nAño: {self.anio}"
+
+# furgon1 = Camion("ABC123", "Mercedes", 1000, 2020)
+# furgon4 = Camion("ABC123", "VW", 1000, 2020)
+
+### F - Creá un pequeño menú que te permita:
+
+### Registrar un nuevo camión.
+### Modificar la carga de un camión.
+### Mostrar por terminal la lista de camiones registrados, del más antiguo al más moderno.
+### Mostrar por terminal la marca que más veces fue registrada.
+
+class Camion:
+    dic_camiones = {}
+    lista_patentes = set()    
+
+    def __init__(self, patente, marca, carga, anio):
+        self.patente = patente
+        self.marca = marca
+        self.carga = carga
+        self.anio = anio
+  
+    def __eq__(self, other):
+        if isinstance(other, Camion):
+            return (self.patente == other.patente)
+
+    def __str__(self):
+        return f"{self.patente} | Carga: {self.carga} Marca: {self.marca} Año: {self.anio}"
+
+    def mostrar_camiones():
+        print('Lista de los camiones:')
+        for c in sorted(Camion.dic_camiones.values(), key=lambda x: x.anio):
+            print(c)
+        
+    def add_camiones():
+        patente = input('Indicar patente: ')
+        if patente in Camion.lista_patentes:
+            raise ValueError(f'Error: La patente asignada ya está en uso')
+        marca = input('Indicar marca: ')
+        carga = int(input('Indicar carga: '))
+        anio = int(input('Indicar año: '))
+
+        nuevo = Camion(patente, marca, carga, anio)
+        Camion.dic_camiones[patente] = nuevo
+        Camion.lista_patentes.add(patente)
+       
+        return ('Nuevo camion agregado')
+    
+    def modificar_carga():
+        patente = input("Ingrese la patente del camión: ")
+        if patente in Camion.dic_camiones:
+            nueva_carga = int(input('Indica nueva carga'))
+            Camion.dic_camiones[patente].carga = nueva_carga
+            print(f"Carga modificada para el camión {patente}.")
+    
+    def top_marca_registrada():
+        marcas = []
+        for i in Camion.dic_camiones.values():
+            if i.marca not in marcas:
+                marcas.append(i.marca[0])
+        max = 0
+        marca_max = ''
+        for m in marcas:
+            cant = marcas.count(m)
+            if cant > max:
+                max = cant
+                marca_max = m
+        
+        print('La marca con más unidades es: ',marca_max)
+        
+def menu():
+    while True:
+        print("\n--- MENÚ CAMIONES ---")
+        print("1. Registrar nuevo camión")
+        print("2. Modificar carga de un camión")
+        print("3. Mostrar lista de camiones")
+        print("4. Mostrar marca más registrada")
+        print("5. Salir")
+
+        opcion = int(input("\nSeleccione una opción: "))
+
+        if opcion == 1:
+            ret = Camion.add_camiones()
+            print(ret)
+
+        if opcion == 3:
+            Camion.mostrar_camiones()
+
+        if opcion == 2:
+            Camion.modificar_carga()
+
+        if opcion == 4:
+            Camion.top_marca_registrada()
+            
+        if opcion == 5:
+            break
+            
+menu()
--- ejercicio2.py
+++ ejercicio2.py
@@ -10,3 +10,122 @@
 # - Crear otra clase para un componente (ej: Disco, RAM, etc.) con su propio __init__, __str__ y al menos un método.
 # 
 # ¡No olvides probar todos los métodos y comentar tu criterio para los valores
+
+class Memoria:
+
+    dic_memorias = {}
+
+    def __init__(self, nro_serie, tipo, capacidad_tot, capacidad_res):
+        try:
+            capacidad_tot = int(capacidad_tot)
+            capacidad_res = int(capacidad_res)
+
+            self.nro_serie = nro_serie
+            self.tipo = tipo
+            self.capacidad_tot = capacidad_tot
+            self.capacidad_res = capacidad_res
+            self.archivos = []
+
+            if nro_serie not in Memoria.dic_memorias:
+                    Memoria.dic_memorias[nro_serie] = {
+                        "tipo": tipo,
+                        "capacidad_tot": capacidad_tot,
+                        "capacidad_res": capacidad_res,
+                    }
+        except:
+            print(f'ERROR: No se cargó correctamente los datos de la memoria {nro_serie}')
+    
+    def __str__(self):
+        return (f'|{self.nro_serie} - Tipo: {self.tipo} Capacidad Total: {self.capacidad_tot} Capacidad Restante: {self.capacidad_res}')
+    
+    def write():
+        nro = int(input('Ingresar numero de serie de la memoria: '))
+        nom = input('Ingresar nombre del archivo: ')
+        tam = int(input('Ingresar tamaño del archivo (en GB): '))
+        dif = Memoria.dic_memorias[nro]['capacidad_tot'] - tam
+
+        if dif < 0 :
+            print('No hay suficiente espacio en el disco')
+            return
+
+        Memoria.dic_memorias[nro]['capacidad_res'] = dif
+        Computadora.dic_computadoras[nro]['memoria'] = dif
+        print(f"Se ha cargado correctamente el archivo {nom}, memoria restante {dif}GB")
+        Computadora.dic_computadoras[nro]['archivos'].append(nom)
+
+    
+    def get_capacidad(self):
+        return (self.capacidad_tot)
+
+class Computadora: 
+    dic_computadoras = {}
+
+    def __init__(self, nro_serie , marca , modelo, procesador, pantalla, ram, memoria, os = 'Desconocido',archivos = None):
+        try:
+            nro_serie = int(nro_serie)
+            self.nro_serie = (nro_serie)
+            self.marca = marca
+            self.modelo = modelo
+            self.procesador = procesador
+            self.pantalla = pantalla
+            self.ram = ram
+            self.memoria = memoria
+            self.os = os
+            if archivos is None:
+                archivos = []
+            self.archivos = archivos
+
+            if nro_serie not in Computadora.dic_computadoras:
+                Computadora.dic_computadoras[nro_serie] = {
+                    "marca": marca,
+                    "modelo": modelo,
+                    "procesador": procesador,
+                    "pantalla": pantalla,
+                    "ram": ram,
+                    "memoria": memoria,
+                    "os": os,
+                    "archivos": archivos,
+                }
+        except:
+            print(f'ERROR: No se cargó correctamente los datos de la computadora {nro_serie}')
+
+    def __str__(self):
+        return (f'| {self.nro_serie} - Marca: {self.marca} Modelo: {self.modelo} Procesador: {self.procesador} Pantalla: {self.pantalla} Ram: {self.ram} Memoria: {self.memoria.capacidad_tot} OS: {self.os}')
+    
+    def updateOS():
+        comp = int(input(f'Indicar el numero de serie de la computadora: '))
+        act_os = input(f'Indicar el nuevo OS la computadora: ')
+        Computadora.dic_computadoras[comp]['os'] = act_os
+        print(f"OS actualizado correctamente para la computadora {comp}")
+
+    def addRam():
+        comp = int(input(f'Indicar el numero de serie de la computadora: '))
+        act_ram = Computadora.dic_computadoras[comp]['ram']
+        ram_agg = int(input(f'Indicar la camntidad de GB de ram a agregar: '))
+        tot = act_ram + ram_agg
+        Computadora.dic_computadoras[comp]['ram'] = tot
+        print(f"Ram agregada. Cantidad total {tot}")
+    
+    def getComputadoras():
+        for nro_serie, datos in Computadora.dic_computadoras.items():
+            print(f"\nComputadora {nro_serie}:")
+            for clave, valor in datos.items():
+                if isinstance(valor, Memoria):
+                    print(f"  {clave}: {valor.capacidad_tot}")
+                else:
+                    print(f"  {clave}: {valor}")
+
+mem1 = Memoria(1, 'HDD', 1000, 1000)
+mem2 = Memoria(2, 'SSD', 120, 120)
+mem3 = Memoria(3, 'SSD', 120, 120)     
+
+com1 = Computadora(1,'Lenovo','ZenBook','I5',14,8,mem1,'Win 11')
+com2 = Computadora(2,'Apple','MacBook','M3',14,1000,mem2)
+com3 = Computadora(3,'HP','EliteBook','R7',17,120,mem3)
+
+
+
+#Computadora.updateOS()
+#Computadora.addRam()
+Memoria.write()
+Computadora.getComputadoras()
